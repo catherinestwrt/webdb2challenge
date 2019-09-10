392754623133b98afe4d03ed820dc23079d816c3
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-10 15:17:06 -0700

    table addition migration fleshed out

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,10 @@ module.exports = {
     connection: {
       filename: './data/carData.bd3'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    }
   },
 
   // staging: {
