ef19d9a0d658d13493d959ef093862127ab20526
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-09-10 15:55:38 -0700

    get all cars api

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const db = require('./data/dbConfig');
 const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.status(200).send({hello: "world"});
+  db.select().table('cars')
+    .then(dbRes => {
+      console.log(dbRes);
+      res.status(200).send({ cars: dbRes });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500);
+    });
 });
 
 module.exports = server;
